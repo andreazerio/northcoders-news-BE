10579c9602f1057fdd7519f4b9d834517fbe4130
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-11-11 16:39:55 +0000

    setting up config and environments

---
+++ config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    DB: {
+      test: 'mongodb://localhost/northcoders-news-api-test',
+      dev: process.env.MONGO_URI
+    },
+    PORT: {
+      test: 3090,
+      dev: process.env.PORT
+    }
+  };
