8f4dcb025a2e910cb69f01e06b893d8c48374bef
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-11-12 10:59:46 +0000

    adding CommentSchema to models/comments

--- models/articles.js
+++ models/articles.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ArticleSchema = new Schema({
+let ArticleSchema = new Schema({
   title: {
     type: String,
     required: true
