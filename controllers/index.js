dc2e62b55bd374b57f37b3557a644fb95596fbc2
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-11-13 12:57:31 +0000

    testing DELETE /api/comments/:comment_id returns a 404 error when given an invalid comment id

--- controllers/index.js
+++ controllers/index.js
@@ -1,63 +1,63 @@
-const {Topics, Articles, Comments} = require('../models/models.js');
+const { Topics, Articles, Comments } = require('../models/models.js');
 
 const getTopics = (req, res, next) => {
     Topics.find()
-    .then(topics => {
-        res.status(200).send({topics});
-    })
-    .catch(err => {
-     if (err) next(err)
-    });
+        .then(topics => {
+            res.status(200).send({ topics });
+        })
+        .catch(err => {
+            if (err) next(err)
+        });
 };
 
 const getArticlesByTopic = (req, res, next) => {
     const topic_id = req.params.topic_id;
-    Articles.find({belongs_to: topic_id})
-    .then(articles => {
-        if (articles.length === 0) return next({status: 404, message: 'topic not found'})
-        res.status(200).send({articles})
-    })
-    .catch(err => {
-        if (err) next(err);
-    });
+    Articles.find({ belongs_to: topic_id })
+        .then(articles => {
+            if (articles.length === 0) return next({ status: 404, message: 'topic not found' })
+            res.status(200).send({ articles })
+        })
+        .catch(err => {
+            if (err) next(err);
+        });
 };
 
 const getArticles = (req, res, next) => {
     Articles.find()
-    .then(articles => {
-        res.status(200).send({articles})
-    })
-    .catch(err => {
-        if (err) next(err)
-    });
+        .then(articles => {
+            res.status(200).send({ articles })
+        })
+        .catch(err => {
+            if (err) next(err)
+        });
 };
 
 const getCommentsByArticleId = (req, res, next) => {
     const article_id = req.params.article_id;
-    Comments.find({belongs_to: article_id})
-    .then(comments => {
-        if (comments.length === 0) return next({status: 404, message: 'article not found'})
-        res.status(200).send({comments});
-    })
-    .catch(err => {
-        if (err.name === 'CastError')  next({status: 404, message: 'article_id not valid'})
-        next(err)
-    });
+    Comments.find({ belongs_to: article_id })
+        .then(comments => {
+            if (comments.length === 0) return next({ status: 404, message: 'article not found' })
+            res.status(200).send({ comments });
+        })
+        .catch(err => {
+            if (err.name === 'CastError') next({ status: 404, message: 'article_id not valid' })
+            next(err)
+        });
 };
 
 const addComment = (req, res, next) => {
-const postBody = req.body.comment;
-const article_id = req.params.article_id;
-if (/^\s*$/g.test(postBody)) return next({status: 400, message: 'comment not valid - provide comment body'});
-const comment = new Comments({
-    body: postBody,
-    belongs_to: article_id
-})
-.save()
-.then(comment => {
-    res.status(201).send({comment})
-})
-.catch(err => next(err));
+    const postBody = req.body.comment;
+    const article_id = req.params.article_id;
+    if (/^\s*$/g.test(postBody)) return next({ status: 400, message: 'comment not valid - provide comment body' });
+    const comment = new Comments({
+        body: postBody,
+        belongs_to: article_id
+    })
+        .save()
+        .then(comment => {
+            res.status(201).send({ comment })
+        })
+        .catch(err => next(err));
 };
 
 const voteArticle = (req, res, next) => {
@@ -65,17 +65,17 @@ const voteArticle = (req, res, next) => {
     const query = req.query.vote;
     let increment;
 
-    if(query === 'up') increment = 1;
-    else if(query === 'down') increment = -1;
+    if (query === 'up') increment = 1;
+    else if (query === 'down') increment = -1;
 
-    Articles.findByIdAndUpdate(article_id, {$inc: {votes: increment}}, {new: true})
-    .then(article => {
-        res.send({article})
-    })
-    .catch(err => {
-        if (err.name === 'CastError') return next({status: 404, message: 'article not found'});
-        next(err);
-    });
+    Articles.findByIdAndUpdate(article_id, { $inc: { votes: increment } }, { new: true })
+        .then(article => {
+            res.send({ article })
+        })
+        .catch(err => {
+            if (err.name === 'CastError') return next({ status: 404, message: 'article not found' });
+            next(err);
+        });
 };
 
 const voteComment = (req, res, next) => {
@@ -83,27 +83,30 @@ const voteComment = (req, res, next) => {
     const query = req.query.vote;
     let increment;
 
-    if(query === 'up') increment = 1;
-    else if(query === 'down') increment = -1;
+    if (query === 'up') increment = 1;
+    else if (query === 'down') increment = -1;
 
-    Comments.findByIdAndUpdate(comment_id, {$inc: {votes: increment}}, {new: true})
-    .then(comment => {
-        res.send({comment})
-    })
-    .catch(err => {
-        if (err.name === 'CastError') return next({status: 404, message: 'comment not found'});
-        next(err);
-    });
+    Comments.findByIdAndUpdate(comment_id, { $inc: { votes: increment } }, { new: true })
+        .then(comment => {
+            res.send({ comment })
+        })
+        .catch(err => {
+            if (err.name === 'CastError') return next({ status: 404, message: 'comment not found' });
+            next(err);
+        });
 };
 
 const deleteComment = (req, res, next) => {
     const comment_id = req.params.comment_id;
     Comments.findByIdAndRemove(comment_id)
-    .exec()
-    .then(comment => {
-        res.status(204).send();
-    })
-    .catch(err => next(err));
+        .exec()
+        .then(comment => {
+            res.status(204).send();
+        })
+        .catch(err => {
+            if (err.name === 'CastError') return next({ status: 404, message: 'comment not found' });
+            next(err);
+        });
 };
 
-module.exports = {getTopics, getArticlesByTopic, getArticles, getCommentsByArticleId, addComment, voteArticle, voteComment, deleteComment}
+module.exports = { getTopics, getArticlesByTopic, getArticles, getCommentsByArticleId, addComment, voteArticle, voteComment, deleteComment }
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -9,128 +9,128 @@ describe('api', () => {
     let updatedData;
     beforeEach(() => {
         return mongoose.connection.dropDatabase()
-        .then(testData)
-        .then(data => {
-            updatedData = data;
-        })
-        .catch(error => console.error)
+            .then(testData)
+            .then(data => {
+                updatedData = data;
+            })
+            .catch(error => console.error)
     });
     describe('GET api', () => {
         it('returns header with status code of 200', () => {
-          return request(app)
-            .get('/api')
-            .expect(200)
-            .then(({body}) => {
-              expect(body).to.eql({
-                status: 'root path working'
-              });
-            });
+            return request(app)
+                .get('/api')
+                .expect(200)
+                .then(({ body }) => {
+                    expect(body).to.eql({
+                        status: 'root path working'
+                    });
+                });
         });
-      });
-      describe('GET /api/topics', () => {
+    });
+    describe('GET /api/topics', () => {
         it('returns an array of topics with a status code of 200', () => {
-          return request(app)
-            .get('/api/topics')
-            .expect(200)
-            .then(res => {
-              expect(res.body.topics).to.be.an('array');
-              expect(res.body.topics.length).to.equal(3);
-              expect(res.body.topics[0].slug).to.be.a('string');
-            });
+            return request(app)
+                .get('/api/topics')
+                .expect(200)
+                .then(res => {
+                    expect(res.body.topics).to.be.an('array');
+                    expect(res.body.topics.length).to.equal(3);
+                    expect(res.body.topics[0].slug).to.be.a('string');
+                });
         });
-      });
-      describe('GET api/topics/:topic_id/articles', () => {
-        it('returns an array of topics with a status code of 200', () => { 
-          const topic = updatedData.topics[2].slug;
-          return request(app)
-            .get(`/api/topics/${topic}/articles`)
-            .expect(200)
-            .then(res => {
-              const articles = res.body.articles;
-              expect(articles).to.be.an('array');
-              expect(articles[0].belongs_to).to.equal(topic);
-              expect(articles[0].title).to.equal('Cats are great')
-            });
+    });
+    describe('GET api/topics/:topic_id/articles', () => {
+        it('returns an array of topics with a status code of 200', () => {
+            const topic = updatedData.topics[2].slug;
+            return request(app)
+                .get(`/api/topics/${topic}/articles`)
+                .expect(200)
+                .then(res => {
+                    const articles = res.body.articles;
+                    expect(articles).to.be.an('array');
+                    expect(articles[0].belongs_to).to.equal(topic);
+                    expect(articles[0].title).to.equal('Cats are great')
+                });
         });
         it('returns a 404 error status code if parameter is not a valid topic', () => {
             return request(app)
-              .get('/api/topics/andrea/articles')
-              .expect(404)
-              .then(res => {
-                const error = res.body.message;
-                expect(error).to.equal('topic not found');
-              });
-          });
-      });
-      describe('GET api/articles', () => {
-          it('returns an array of articles with a status code of 200', () => {
-              return request(app)
-              .get('/api/articles')
-              .expect(200)
-              .then(res => {
-                expect(res.body.articles).to.be.an('array');
-                expect(res.body.articles.length).to.equal(2);
-                expect(res.body.articles[1].title).to.equal('Football is fun')
-                expect(res.body.articles[0].belongs_to).to.be.a('string');
-              });
-          });
-      });
-      describe('GET /api/articles/:article_id/comments', () => {
-          it('returns an array of comments with a status code of 200', () => {
+                .get('/api/topics/andrea/articles')
+                .expect(404)
+                .then(res => {
+                    const error = res.body.message;
+                    expect(error).to.equal('topic not found');
+                });
+        });
+    });
+    describe('GET api/articles', () => {
+        it('returns an array of articles with a status code of 200', () => {
+            return request(app)
+                .get('/api/articles')
+                .expect(200)
+                .then(res => {
+                    expect(res.body.articles).to.be.an('array');
+                    expect(res.body.articles.length).to.equal(2);
+                    expect(res.body.articles[1].title).to.equal('Football is fun')
+                    expect(res.body.articles[0].belongs_to).to.be.a('string');
+                });
+        });
+    });
+    describe('GET /api/articles/:article_id/comments', () => {
+        it('returns an array of comments with a status code of 200', () => {
             const article_id = updatedData.articles[0]._id;
             return request(app)
-              .get(`/api/articles/${article_id}/comments`)
-              .expect(200)
-              .then((res) => {
-                const comments = res.body.comments;
-                expect(comments).to.be.an('array');
-                expect(comments[0].belongs_to).to.equal(article_id.toString());
-                expect(comments[0].created_by).to.equal('northcoder')
-              });
-          });
-         it('returns a 404 error if parameter is a non existent id in the right format', () => {
+                .get(`/api/articles/${article_id}/comments`)
+                .expect(200)
+                .then((res) => {
+                    const comments = res.body.comments;
+                    expect(comments).to.be.an('array');
+                    expect(comments[0].belongs_to).to.equal(article_id.toString());
+                    expect(comments[0].created_by).to.equal('northcoder')
+                });
+        });
+        it('returns a 404 error if parameter is a non existent id in the right format', () => {
             return request(app)
-            .get('/api/articles/41224d776a326fb40f000001/comments')
-            .expect(404)
-            .then(res => {
-              const error = res.body.message;
-              expect(error).to.equal('article not found');
-            });
-         });
-         it('returns a 404 error if parameter is not a valid article id format', () => {
+                .get('/api/articles/41224d776a326fb40f000001/comments')
+                .expect(404)
+                .then(res => {
+                    const error = res.body.message;
+                    expect(error).to.equal('article not found');
+                });
+        });
+        it('returns a 404 error if parameter is not a valid article id format', () => {
             return request(app)
-            .get('/api/articles/andrea/comments')
-            .expect(404)
-            .then(res => {
-              const error = res.body.message;
-              expect(error).to.equal('article_id not valid');
-            });
-         });
-      });
-      describe('POST /api/articles/:article_id/comments', () => {
-          it('returns the comment poste by the user with a status code of 201', () => {
+                .get('/api/articles/andrea/comments')
+                .expect(404)
+                .then(res => {
+                    const error = res.body.message;
+                    expect(error).to.equal('article_id not valid');
+                });
+        });
+    });
+    describe('POST /api/articles/:article_id/comments', () => {
+        it('returns the comment poste by the user with a status code of 201', () => {
             const article_id = updatedData.articles[0]._id;
             const comment = 'This comment is to test the api';
             return request(app)
-            .post(`/api/articles/${article_id}/comments`)
-            .send({comment})
-            .expect(201)
-            .then(res => {
-                const comment_test = res.body.comment.body;
-                expect(comment_test).to.equal(comment);
-            });
-          });
-          it('returns a 400 error if the body of the comment is an empty string or a string of whitespaces', () => {
+                .post(`/api/articles/${article_id}/comments`)
+                .send({ comment })
+                .expect(201)
+                .then(res => {
+                    const comment_test = res.body.comment.body;
+                    expect(comment_test).to.equal(comment);
+                });
+        });
+        it('returns a 400 error if the body of the comment is an empty string or a string of whitespaces', () => {
             const article_id = updatedData.articles[0]._id;
             const comment = '                    ';
             return request(app)
-              .post(`/api/articles/${article_id}/comments`)
-              .send({comment})
-              .expect(400)
-              .then(res => {
-                const error = res.body.message;
-                expect(error).to.equal('comment not valid - provide comment body');
-            });
+                .post(`/api/articles/${article_id}/comments`)
+                .send({ comment })
+                .expect(400)
+                .then(res => {
+                    const error = res.body.message;
+                    expect(error).to.equal('comment not valid - provide comment body');
+                });
         });
     });
     describe('PUT /api/articles/:article_id?vote=down', () => {
@@ -138,95 +138,104 @@ describe('api', () => {
             const article_id = updatedData.articles[0]._id;
             const votes = updatedData.articles[0].votes;
             return request(app)
-            .put(`/api/articles/${article_id}?vote=down`)
-            .expect(200)
-            .then(res => {
-            const newVotes = res.body.article.votes;
-            expect(newVotes).to.equal(votes - 1);
-            });
+                .put(`/api/articles/${article_id}?vote=down`)
+                .expect(200)
+                .then(res => {
+                    const newVotes = res.body.article.votes;
+                    expect(newVotes).to.equal(votes - 1);
+                });
         });
         it('returns a 404 error if parameter is not a valid article id', () => {
             return request(app)
-              .put('/api/articles/andrea?vote=down')
-              .expect(404)
-              .then((res) => {
-                  const error = res.body.message;
-                expect(error).to.equal('article not found');
-              });
-          });
+                .put('/api/articles/andrea?vote=down')
+                .expect(404)
+                .then((res) => {
+                    const error = res.body.message;
+                    expect(error).to.equal('article not found');
+                });
+        });
     });
     describe('PUT /api/articles/:article_id?vote=up', () => {
         it('decreses the number of votes for the article selected and return a status code of 200', () => {
             const article_id = updatedData.articles[0]._id;
             const votes = updatedData.articles[0].votes;
             return request(app)
-            .put(`/api/articles/${article_id}?vote=up`)
-            .expect(200)
-            .then(res => {
-            const newVotes = res.body.article.votes;
-            expect(newVotes).to.equal(votes + 1);
-            });
+                .put(`/api/articles/${article_id}?vote=up`)
+                .expect(200)
+                .then(res => {
+                    const newVotes = res.body.article.votes;
+                    expect(newVotes).to.equal(votes + 1);
+                });
         });
         it('returns a 404 error if parameter is not a valid article id', () => {
             return request(app)
-              .put('/api/articles/andrea?vote=up')
-              .expect(404)
-              .then((res) => {
-                  const error = res.body.message;
-                expect(error).to.equal('article not found');
-              });
-          });
+                .put('/api/articles/andrea?vote=up')
+                .expect(404)
+                .then((res) => {
+                    const error = res.body.message;
+                    expect(error).to.equal('article not found');
+                });
+        });
     });
     describe('PUT /api/comments/:comment_id?vote=down', () => {
         it('decreses the number of votes for the comment selected and return a status code of 200', () => {
             const comment_id = updatedData.comments[0]._id;
             const votes = updatedData.comments[0].votes;
             return request(app)
-            .put(`/api/comments/${comment_id}?vote=down`)
-            .expect(200)
-            .then(res => {
-            const newVotes = res.body.comment.votes;
-            expect(newVotes).to.equal(votes - 1);
-            });
+                .put(`/api/comments/${comment_id}?vote=down`)
+                .expect(200)
+                .then(res => {
+                    const newVotes = res.body.comment.votes;
+                    expect(newVotes).to.equal(votes - 1);
+                });
         });
         it('returns a 404 error if parameter is not a valid comment id', () => {
             return request(app)
-              .put('/api/comments/andrea?vote=down')
-              .expect(404)
-              .then((res) => {
-                  const error = res.body.message;
-                expect(error).to.equal('comment not found');
-              });
-          });
+                .put('/api/comments/andrea?vote=down')
+                .expect(404)
+                .then((res) => {
+                    const error = res.body.message;
+                    expect(error).to.equal('comment not found');
+                });
+        });
     });
     describe('PUT /api/comments/:comment_id?vote=up', () => {
         it('decreses the number of votes for the comment selected and return a status code of 200', () => {
             const comment_id = updatedData.comments[0]._id;
             const votes = updatedData.comments[0].votes;
             return request(app)
-            .put(`/api/comments/${comment_id}?vote=up`)
-            .expect(200)
-            .then(res => {
-            const newVotes = res.body.comment.votes;
-            expect(newVotes).to.equal(votes + 1);
-            });
+                .put(`/api/comments/${comment_id}?vote=up`)
+                .expect(200)
+                .then(res => {
+                    const newVotes = res.body.comment.votes;
+                    expect(newVotes).to.equal(votes + 1);
+                });
         });
         it('returns a 404 error if parameter is not a valid comment id', () => {
             return request(app)
-              .put('/api/comments/andrea?vote=up')
-              .expect(404)
-              .then((res) => {
-                  const error = res.body.message;
-                expect(error).to.equal('comment not found');
-              });
-          });
+                .put('/api/comments/andrea?vote=up')
+                .expect(404)
+                .then((res) => {
+                    const error = res.body.message;
+                    expect(error).to.equal('comment not found');
+                });
+        });
     });
     describe('DELETE /api/comments/:comment_id', () => {
         it('returns with a 204 status code', () => {
             const comment_id = updatedData.comments[0]._id;
             return request(app)
-              .delete(`/api/comments/${comment_id}`)
-              .expect(204);
+                .delete(`/api/comments/${comment_id}`)
+                .expect(204);
+        });
+        it('returns a 404 error if parameter is not a valid comment id', () => {
+            return request(app)
+                .delete('/api/comments/andrea')
+                .expect(404)
+                .then((res) => {
+                    const error = res.body.message;
+                    expect(error).to.equal('comment not found');
+                });
         });
     });
 });
