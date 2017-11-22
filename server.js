7d6fef3f0909a651623ff3153bf2200661710d29
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-11-22 13:47:18 +0000

    adding static view to backend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const config = require('./config');
 const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 const apiRouter = require('./routers/api');
+const cors = require('cors');
 
 mongoose.Promise = Promise;
 
@@ -15,12 +16,14 @@ mongoose.connect(db, {useMongoClient: true})
   .catch(err => console.log('connection failed', err));
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use('/',express.static('public'));
 app.use('/api', apiRouter);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use((err, req, res, next) => {
+  if(err.status === 404) return res.status(404).send({message: err.message});
+  if(err.status === 400) return res.status(400).send({message: err.message});
+  else return next(err);
 });
 app.use('/*', (req, res) => {
   res.status(404).send({message: 'Page not found'});
