d7491c99e7150df23160879892a54ece74e6e566
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-12-01 15:17:10 +0000

    extracting topics controllers in separate file

---
+++ controllers/topicsController.js
@@ -0,0 +1,13 @@
+const Topics = require('../models/models.js');
+
+
+const getTopics = (req, res, next) => {
+    Topics.find()
+        .then(topics => {
+            res.status(200).send({ topics });
+        })
+        .catch(err => next(err));
+};
+
+
+module.exports = {getTopics}
