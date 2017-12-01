31017f0dd502ecc3fe06cac389c8c858e07afa0d
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-12-01 15:37:40 +0000

    extracting comments controllers into separate file

---
+++ controllers/commentsController.js
@@ -0,0 +1,34 @@
+const {Comments} = require('../models/models.js');
+
+const voteComment = (req, res, next) => {
+    const comment_id = req.params.comment_id;
+    const query = req.query.vote;
+    let increment;
+
+    if (query === 'up') increment = 1;
+    else if (query === 'down') increment = -1;
+
+    Comments.findByIdAndUpdate(comment_id, { $inc: { votes: increment } }, { new: true })
+        .then(comment => {
+            res.send({ comment })
+        })
+        .catch(err => {
+            if (err.name === 'CastError') return next({ status: 404, message: 'comment not found' });
+            next(err);
+        });
+};
+
+const deleteComment = (req, res, next) => {
+    const comment_id = req.params.comment_id;
+    Comments.findByIdAndRemove(comment_id)
+        .exec()
+        .then(comment => {
+            res.status(204).send();
+        })
+        .catch(err => {
+            if (err.name === 'CastError') return next({ status: 404, message: 'comment not found' });
+            next(err);
+        });
+};
+
+module.exports = {voteComment, deleteComment}
--- routers/commentsRouter.js
+++ routers/commentsRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {voteComment, deleteComment} = require('../controllers/index');
+const {voteComment, deleteComment} = require('../controllers/commentsController');
 
 router.route('/:comment_id')
 .put(voteComment)
